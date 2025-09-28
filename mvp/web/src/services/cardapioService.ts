342a2807d12a58ddb070926fb2ae3afb0bb768f0
Patrick Duarte <patrickichigo1475@gmail.com>
2025-09-28 00:52:44 -0300

    [FIX] Ajustando para pegar o parametro e pasasndo para o company dinamicamente

--- mvp/web/src/services/cardapioService.ts
+++ mvp/web/src/services/cardapioService.ts
@@ -1,11 +1,14 @@
-const x =
-    "https://gnzfdeubnee3waor43psjpnjoi0ielfw.lambda-url.us-east-2.on.aws/?company=pedeaqui";
+const path = window.location.pathname; // "/pede-aqui/girafas"
+const prefix = "/pede-aqui/";
+const value = path.startsWith(prefix) ? path.slice(prefix.length) : null;
+const lambdaUrl = 'https://gnzfdeubnee3waor43psjpnjoi0ielfw.lambda-url.us-east-2.on.aws/?company='
 
 export const cardapioService = {
     getCardapio: async () => {
-        const response = await fetch(x);
+        console.log(value);
+        const response = await fetch(lambdaUrl+`${value}`);
+        console.log(response);
         if (!response.ok) {
-            console.error("Erro ao fazer GET para a URL", x);
             return [];
         }
         return response.json();
