b4379bf04dd77b2174e1c5748481a966ee2adb9b
Patrick Duarte <patrickichigo1475@gmail.com>
2025-09-27 23:46:03 -0300

    [FEAT] Adicionando a opção de deploy no github

--- mvp/web/src/App.tsx
+++ mvp/web/src/App.tsx
@@ -1,10 +1,10 @@
 import { Router } from "./Router";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 export function App() {
   return (
-      <BrowserRouter>
+      <HashRouter>
           <Router />
-      </BrowserRouter>
+      </HashRouter>
   );
 }
